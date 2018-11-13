95296d6de922369d736440e79fbe9c26ee705f6f
Vikrant Gajria <vikrantgajria@gmail.com>
2018-11-13 18:24:03 +0530

    Enforcing HTTPS

--- app/src/index.js
+++ app/src/index.js
@@ -13,6 +13,24 @@ import 'animate.css';
 
 import * as serviceWorker from './serviceWorker';
 
+// ----- Enforce HTTPS
+
+const isLocalhost = Boolean(
+    window.location.hostname === 'localhost' ||
+      // [::1] is the IPv6 localhost address.
+      window.location.hostname === '[::1]' ||
+      // 127.0.0.1/8 is considered localhost for IPv4.
+      window.location.hostname.match(
+        /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+      )
+);
+
+if (!isLocalhost && window.location.protocol !== 'https:') {
+    window.location.href = 'https:' + window.location.href.substring(window.location.protocol.length);
+}
+
+// -----
+
 // redux
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
