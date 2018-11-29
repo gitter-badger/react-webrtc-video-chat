75a969fdc34a5af2d6c0fc1bcee1901c2a04b63a
Vikrant Gajria <vikrantgajria@gmail.com>
2018-11-29 21:55:20 +0530

    🎣 Change Navbar to functional component
    I used a class component because I was not sure if I was going to
    convert it to a class component down the line.
    
    Now with hooks I don't need to worry about that.

--- app/src/components/Navbar.js
+++ app/src/components/Navbar.js
@@ -1,35 +1,33 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Menu, Sticky } from 'semantic-ui-react';
 
 import { connect } from 'react-redux';
 import uiActions from '../actions/uiActions';
 
-class Navbar extends Component {
-    render = () => (
-        <Sticky>
-            <Menu inverted color='violet'>
-                <Menu.Item
-                    icon='user circle'
-                    name='Shakal'
-                />
-    
-                <Menu.Item
-                    icon='users'
-                    name='Online Users'
-                    onClick={this.props.toggleList}
-                    active={this.props.isOnlineListOpen}
-                />
-    
-                <Menu.Item
-                    icon='bug'
-                    position='right'
-                    onClick={this.props.toggleDebugDialog}
-                    active={this.props.isDebugDialogOpen}
-                />
-            </Menu>
-        </Sticky>
-    )
-}
+const Navbar = (props) => (
+    <Sticky>
+        <Menu inverted color='violet'>
+            <Menu.Item
+                icon='user circle'
+                name='Shakal'
+            />
+
+            <Menu.Item
+                icon='users'
+                name='Online Users'
+                onClick={props.toggleList}
+                active={props.isOnlineListOpen}
+            />
+
+            <Menu.Item
+                icon='bug'
+                position='right'
+                onClick={props.toggleDebugDialog}
+                active={props.isDebugDialogOpen}
+            />
+        </Menu>
+    </Sticky>
+);
 
 const mapStateToProps = store => ({
     isOnlineListOpen: store.ui.openUsersList,
@@ -40,5 +38,5 @@ const mapDispatchToProps = dispatch => ({
     toggleList: _ => dispatch(uiActions.TOGGLE_USER_LIST()),
     toggleDebugDialog: _ => dispatch(uiActions.TOGGLE_DEBUG_DIALOG()),
 });
-    
+
 export default connect(mapStateToProps, mapDispatchToProps)(Navbar);
